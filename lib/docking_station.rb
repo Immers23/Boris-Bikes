445164bb29720b6cc6d353614ba9d40e1f983472
benauld <ben@portobello-consultancy.co.uk>
2019-12-11 16:18:15 +0000

    Exercise 14

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,19 +1,27 @@
 require_relative 'bike'
 
 class DockingStation
-  attr_reader :bike
+  attr_reader :bikes
+
+    def initialize
+      @bikes = []
+    end
 
   def release_bike
-    fail "No bikes available" unless @bike
-      @bike
+    p "Bikes count:"
+    p @bikes.count
+    p @bikes
+    fail "No bikes available" if @bikes.count == 0
+      p "didn't return"
+      @bikes
   end
 
   def dock(bike)
-    @bike = bike
+    fail "Dock is full" if @bikes.count >= 20
+      @bikes << bike
   end
 
   def bike
-    @bike
+    @bikes
   end
-end 
-
+end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,13 @@
 require 'docking_station'
 
-describe DockingStation do 
+describe DockingStation do
+  it "Starts with 20 bikes in docking station" do
+    bike = Bike.new
+    20.times { subject.dock(bike) }
+    expect{subject.dock(bike)}.to raise_error("Dock is full")
+  end
+
+
   it { is_expected.to respond_to :release_bike }
 
   it 'raises error on .release_bike when docking station is empty' do
@@ -10,8 +17,15 @@ describe DockingStation do
   it 'releases working bikes' do
     bike = Bike.new
     subject.dock(bike)
-    expect(subject.release_bike).to be_working
+    expect(subject.release_bike).to eq [bike]
   end
+
+  it 'Dock second bike' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect { subject.dock(bike)} .to be_working
+  end
+
 
   it 'docks something' do
     bike = Bike.new
