84cee8c21588876b4d0b1dda16b1414d039fbe5f
niki-01 <niki.manoledaki@gmail.com>
2019-12-11 14:53:33 +0000

    Raises exception if docking station is empty

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,7 +3,13 @@ require 'docking_station'
 describe DockingStation do 
   it { is_expected.to respond_to :release_bike }
 
+  it 'raises error on .release_bike when docking station is empty' do
+    expect { subject.release_bike } .to raise_error("No bikes available")
+  end
+
   it 'releases working bikes' do
+    bike = Bike.new
+    subject.dock(bike)
     expect(subject.release_bike).to be_working
   end
 
@@ -12,4 +18,5 @@ describe DockingStation do
     subject.dock(bike)
     expect(subject.bike).to eq bike
   end
+
 end
